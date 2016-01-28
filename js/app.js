dfe63fc51910f99dfe5aa701dfa6a6b6859e6bb6
jean-michel.legrand <Fluent2015>
2016-01-28 10:45:27 +0100

    Add the initial version of the project

---
+++ js/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by jean-michel.legrand on 28/01/2016.
+ */
+function add(x, y) {
+  return x + y ;
+}
+
+console.log('add(2, 3):', add(2, 3)); // should return 5
+console.log('add(char(2), char(3)):' , add('2', '3')); // should not return '23'
+console.log('add(0.1, 0.2):', add(0.1, 0.2)); // should return 0.4
